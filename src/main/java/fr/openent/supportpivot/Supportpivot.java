e91ded77d380c4b012484462d8b0676c4931c43f
Thomas COLENO <thomas.coleno@cgi.com>
2017-12-15 15:43:35 +0100

    Translation of constants

--- src/main/java/fr/openent/supportpivot/Supportpivot.java
+++ src/main/java/fr/openent/supportpivot/Supportpivot.java
@@ -4,6 +4,46 @@ import org.entcore.common.http.BaseServer;
 
 public class Supportpivot extends BaseServer {
 
+    /**
+     * Déclaration des champs JSON du format pivot
+     */
+    public final static String IDIWS_FIELD = "id_iws";
+    public final static String IDENT_FIELD = "id_ent";
+    public final static String IDJIRA_FIELD = "id_jira";
+    public final static String COLLECTIVITY_FIELD = "collectivite";
+    public final static String CREATOR_FIELD = "demandeur";
+    public final static String TICKETTYPE_FIELD = "type_demande";
+    public final static String TITLE_FIELD = "titre";
+    public final static String DESCRIPTION_FIELD = "description";
+    public final static String PRIORITY_FIELD = "priorite";
+    public final static String MODULES_FIELD = "modules";
+    public final static String COMM_FIELD = "commentaires";
+    public final static String ATTACHMENT_FIELD = "pj";
+    public final static String ATTACHMENT_NAME_FIELD = "nom";
+    public final static String ATTACHMENT_CONTENT_FIELD = "contenu";
+    public final static String STATUSIWS_FIELD = "statut_iws";
+    public final static String STATUSENT_FIELD = "statut_ent";
+    public final static String STATUSJIRA_FIELD = "statut_jira";
+    public final static String DATE_CREA_FIELD = "date_creation";
+    public final static String DATE_RESOIWS_FIELD = "date_resolution_iws";
+    public final static String DATE_RESOENT_FIELD = "date_resolution_ent";
+    public final static String DATE_RESOJIRA_FIELD = "date_resolution_jira";
+    public final static String ATTRIBUTION_FIELD = "attribution";
+
+    /**
+     * Champs obligatoires
+     */
+    public static final String[] IWS_MANDATORY_FIELDS = {
+            IDIWS_FIELD,
+            COLLECTIVITY_FIELD,
+            CREATOR_FIELD,
+            TITLE_FIELD,
+            DESCRIPTION_FIELD,
+            ATTRIBUTION_FIELD
+            };
+
+
+
 	@Override
 	public void start() {
 		super.start();
