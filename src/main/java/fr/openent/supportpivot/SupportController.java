f9b596e5ef40bcbd223743a9a6c1274f44c74168
COLENO <thomas.coleno@cgi.com>
2018-01-24 09:49:51 +0100

    Bug correction

--- src/main/java/fr/openent/supportpivot/SupportController.java
+++ src/main/java/fr/openent/supportpivot/SupportController.java
@@ -33,7 +33,7 @@ import java.util.Map;
  * /testMail/:mail : Send a test mail to address in parameter
  * /demandeENT : Register a demande from Support module
  */
-class SupportController extends ControllerHelper{
+public class SupportController extends ControllerHelper{
 
     private DemandeService demandeService;
 
